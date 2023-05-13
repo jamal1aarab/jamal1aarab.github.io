dc1e1fa634e6362019c81ec998c3dd7e70ff0327
AARAB Jamal <jamal.aarab.111@gmail.com>
2023-05-13 05:36:48 -0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,11 +59,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Abdellah Aarab";
+            document.title = "Portfolio | Jamal Aarab";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Abdellah Aarab Portfolio";
+            document.title = "Come Back To Jamal Aarab Portfolio";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
     });
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "flutter development", "web development"],
+    strings: ["frontend development","web development","devOps","cloud engineering"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
